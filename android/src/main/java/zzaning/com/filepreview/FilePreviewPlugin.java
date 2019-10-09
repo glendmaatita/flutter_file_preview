05439c375cb2c8ccd1fb296cd40e3b282e8014b5
Glend Maatita <glend.maatita@gmail.com>
2019-10-09 12:28:19 +0700

    Update FilePreviewPlugin.java

--- android/src/main/java/zzaning/com/filepreview/FilePreviewPlugin.java
+++ android/src/main/java/zzaning/com/filepreview/FilePreviewPlugin.java
@@ -33,14 +33,13 @@ public class FilePreviewPlugin implements MethodCallHandler {
         QbSdk.initX5Environment(context, new QbSdk.PreInitCallback() {
             @Override
             public void onCoreInitFinished() {
-                //x5内核初始化完成回调接口，此接口回调并表示已经加载起来了x5，有可能特殊情况下x5内核加载失败，切换到系统内核。
-                TLog.d("初始化X5成功");
+                TLog.d("Finish Initiation");
             }
 
             @Override
             public void onViewInitFinished(boolean b) {
                 //x5內核初始化完成的回调，为true表示x5内核加载成功，否则表示x5内核加载失败，会自动切换到系统内核。
-                TLog.e("**************", "加载内核是否成功:" + b);
+                TLog.e("**************", "Finish View Initiation:" + b);
             }
         });
     }
